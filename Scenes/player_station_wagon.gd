80322de4648c4312401be53b7bde5b01e56fdb5f
kalegar <sanjy33@hotmail.com>
2026-01-12 09:06:46 -0700

    Remake vehicle scene. Modified model in blender to allow moving steering wheel, spedometer needles etc

---
+++ Scenes/player_station_wagon.gd
@@ -0,0 +1,59 @@
+extends VehicleBody3D
+
+var cam_first_person
+var cam_3rd_person
+var door_right : MeshInstance3D
+var door_left : MeshInstance3D
+var steering_wheel : MeshInstance3D
+var steering_wheel_zero_basis
+var spedometer : MeshInstance3D
+var spedometer_zero_basis
+var rpm_meter : MeshInstance3D
+var rpm_zero_basis
+var rpm_angle
+var door_positions_left : Array
+var door_positions_right : Array
+var door_position_indexes : Array
+const MAX_STEER =  0.3
+const ENGINE_POWER = 200
+const BRAKE_POWER = 5.0
+
+func _ready():
+	cam_first_person = get_node("CameraFirstPerson")
+	cam_3rd_person = get_node("Camera3rdPerson")
+	door_right = get_node("Door Right")
+	door_left = get_node("Door Left")
+	steering_wheel = get_node("Steering Wheel")
+	spedometer = get_node("Spedometer Needle")
+	rpm_meter = get_node("RPM Needle")
+	rpm_zero_basis = rpm_meter.basis
+	rpm_angle = 0
+	steering_wheel_zero_basis = steering_wheel.basis
+	spedometer_zero_basis = spedometer.basis
+	door_positions_left = [door_left.transform.basis, door_left.transform.basis.rotated(Vector3(0,1,0), -0.9)]
+	door_positions_right = [door_right.transform.basis, door_right.transform.basis.rotated(Vector3(0,1,0), .9)]
+	door_position_indexes = [0,0]
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _process(delta: float) -> void:
+	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 2.5)
+	engine_force = Input.get_axis("ui_down", "ui_up") * ENGINE_POWER
+	brake = float(Input.is_action_pressed("action_brake")) * BRAKE_POWER
+	spedometer.basis = spedometer_zero_basis.rotated(Vector3(0,0,1),linear_velocity.length()*0.1)
+	steering_wheel.basis = steering_wheel_zero_basis.rotated(Vector3(0,-.2,.8).normalized(),-steering*2.5)
+	rpm_angle = move_toward(rpm_angle, engine_force*.02, delta*3)
+	rpm_meter.basis = rpm_zero_basis.rotated(Vector3(0,0,1),abs(rpm_angle))
+	
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+		
+	if Input.is_action_just_pressed("action_change_view"):
+		cam_first_person.current = !cam_first_person.current
+		
+	if Input.is_action_just_pressed("action_open_doors"):
+		for i in 2:
+			door_position_indexes[i] += 1
+			if door_position_indexes[i] > 1:
+				door_position_indexes[i] = 0
+		door_right.basis = door_positions_right[door_position_indexes[0]]
+		door_left.basis = door_positions_left[door_position_indexes[1]]
